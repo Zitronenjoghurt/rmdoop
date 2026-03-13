b6a664c2d8eebbb506082359e5749982a974878e
Zitronenjoghurt <117850085+Zitronenjoghurt@users.noreply.github.com>
2026-03-13 14:16:41 +0100

    implement duplicate grouping

--- src/cli.rs
+++ src/cli.rs
@@ -4,8 +4,15 @@ use std::path::PathBuf;
 #[derive(Parser)]
 #[command(name = "rmdoop", version)]
 pub struct Cli {
-    #[arg(short, long, required = true, num_args = 1.., value_hint = ValueHint::DirPath)]
-    pub sources: Vec<PathBuf>,
-    #[arg(short, long, required = true, num_args = 1.., value_hint = ValueHint::DirPath)]
+    /// Target paths to scan and potentially delete files from.
+    /// Can be either files or directories (which will be scanned recursively).
+    #[arg(value_hint = ValueHint::AnyPath)]
     pub targets: Vec<PathBuf>,
+    /// Source paths to scan, which will be compared to the target paths and never deleted.
+    /// Can be either files or directories (which will be scanned recursively).
+    #[arg(short, long, value_hint = ValueHint::AnyPath)]
+    pub sources: Vec<PathBuf>,
+    /// If this is true and a group of duplicate files has no source file, a random file from the group will be promoted to a source file.
+    #[arg(short, long)]
+    pub promote_random_duplicate: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use crate::grouping::group_identical;
 use clap::Parser;
 
 mod cli;
+mod grouping;
+mod io;
 #[cfg(test)]
 mod tests;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let cli = cli::Cli::parse();
 
-    println!("{:?}", cli.sources);
-    println!("{:?}", cli.targets);
+    let groups = group_identical(&cli.sources, &cli.targets)?;
+    println!("{groups:#?}");
+
+    Ok(())
 }
